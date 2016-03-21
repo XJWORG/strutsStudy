487af95827f691fd62718d384a829b04fc4f330b
xjwldlover <xjw86@sohu.com>
2016-03-21 23:21:00 +0800

    添加输入校验内容（addFieldError）

---
+++ javaprojs/WebRoot/register.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" import="java.util.*" pageEncoding="GB18030"%>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <base href="<%=basePath%>">
+    
+    <title>My JSP 'input.jsp' starting page</title>
+    
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expires" content="0">    
+    <meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+    <meta http-equiv="description" content="This is my page">
+    <!--
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    -->
+
+  </head>
+  
+<body>
+	<s:form action="register" method="post" >
+		<s:textfield name="username" label="username"></s:textfield>
+		<s:password name="password" label="password"></s:password>
+		<s:password name="repassword" label="re-password"></s:password>
+		<s:textfield name="age" label="age" ></s:textfield>
+		<s:textfield name="birthday" label="birthday"></s:textfield>
+		<s:submit name="submit"></s:submit>
+		<s:reset name="reset"></s:reset>
+	</s:form>
+
+</body>
+</html>
---
+++ javaprojs/src/com/test/action/RegisterAction.java
@@ -0,0 +1,104 @@
+package com.test.action;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+
+	private String username;
+	private String password;
+	private String repassword;
+	private Integer age;
+	private Date birthday;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRepassword() {
+		return repassword;
+	}
+
+	public void setRepassword(String repassword) {
+		this.repassword = repassword;
+	}
+
+	public Integer getAge() {
+		System.out.println("use setAge");
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		System.out.println("use getAge");
+		this.age = age;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		System.out.println("Register Action Execute !");
+		return SUCCESS;
+
+	}
+
+	@Override
+	public void validate() {
+
+		System.out.println("use validate");
+		if (null == this.username || null == this.username) {
+			this.addFieldError("username",
+					"username or password must not be null");
+		} else if (username.length() < 6 || username.length() > 15) {
+			this.addFieldError("username",
+					"length of username should between 6 and 15");
+		}
+
+		if (password.length() < 6 || password.length() > 15) {
+			this.addFieldError("password",
+					"length of password should between 6 and 15");
+		}
+
+		if (null== this.age || this.age > 150 || this.age < 1) {
+			this.addFieldError("age",
+					"value of age should be between 1 and 150");
+		}
+
+		if (null == this.birthday) {
+			this.addFieldError("birthday", "birthday must not be null");
+		} else {
+
+			Calendar birthday = Calendar.getInstance();
+			birthday.setTime(this.birthday);
+
+			Calendar today = Calendar.getInstance();
+			today.setTime(today.getTime());
+
+			if (birthday.after(today)) {
+				this.addFieldError("birthday",
+						"birthday cannot greater than today");
+			}
+		}
+	}
+
+}
